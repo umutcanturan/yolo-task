6e3d874c74d58c2ff5a85b75b0384d661743d210
Umutcan Turan <umutcanturan55@gmail.com>
2022-12-09 22:21:04 +0300

    init project

---
+++ yolo.api/Controllers/InvertController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace yolo.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class InvertController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
+
