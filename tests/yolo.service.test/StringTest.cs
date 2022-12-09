ec70b97dafaa068d28447367183223ddc2004347
Umutcan Turan <umutcanturan55@gmail.com>
2022-12-10 00:30:48 +0300

    test project added for string service. needs to be tested

---
+++ tests/yolo.service.test/StringTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using yolo.service.implementations;
+
+namespace yolo.service.test
+{
+	[TestClass]
+	public class StringTest
+	{
+		public StringTest()
+		{
+		}
+
+		[TestMethod]
+		public void Reverse_Success()
+		{
+			//Arrange
+			string item = "test";
+			var stringService = new StringService();
+			//Act
+			var result = stringService.Reverse(item);
+            //Assert
+            Assert.IsTrue(result == "tset");
+			
+		}
+	}
+}
+
---
+++ tests/yolo.service.test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace yolo.service.test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
